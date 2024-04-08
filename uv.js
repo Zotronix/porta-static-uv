91f2c3732988991d1a48cad64c1269aef7239eee
Zotronix <158109676+Zotronix@users.noreply.github.com>
2024-04-07 20:37:47 -0400

    Update uv.js

--- uv.js
+++ uv.js
@@ -1,6 +1,6 @@
-importScripts('/uv/uv.bundle.js');
-importScripts('/uv/uv.config.js');
-importScripts('/uv/uv.sw.js');
+importScripts('https://raw.githubusercontent.com/Zotronix/porta-static-uv/uv/uv.bundle.js');
+importScripts('https://raw.githubusercontent.com/Zotronix/porta-static-uv/uv/uv.config.js');
+importScripts('https://raw.githubusercontent.com/Zotronix/porta-static-uv/uv/uv.sw.js');
 importScripts('https://arc.io/arc-sw-core.js');
 
 const sw = new UVServiceWorker();
