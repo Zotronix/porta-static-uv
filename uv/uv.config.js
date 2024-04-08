8c57e17d1aef0c6e8844cab3cef535706c144c44
Zotronix <158109676+Zotronix@users.noreply.github.com>
2024-04-07 20:46:21 -0400

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -3,8 +3,8 @@ self.__uv$config = {
     bare:'https://bare.benrogo.net',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: 'https://raw.githubusercontent.com/Zotronix/porta-static-uv/main/uv/uv.handler.js',
-    bundle: 'https://raw.githubusercontent.com/Zotronix/porta-static-uv/main/uv/uv.bundle.js',
-    config: 'https://raw.githubusercontent.com/Zotronix/porta-static-uv/main/uv/uv.config.js',
-    sw: 'https://raw.githubusercontent.com/Zotronix/porta-static-uv/main/uv/uv.sw.js',
+    handler: '/Zotronix/porta-static-uv/main/uv/uv.handler.js',
+    bundle: '/Zotronix/porta-static-uv/main/uv/uv.bundle.js',
+    config: '/Zotronix/porta-static-uv/main/uv/uv.config.js',
+    sw: '/Zotronix/porta-static-uv/main/uv/uv.sw.js',
 };
